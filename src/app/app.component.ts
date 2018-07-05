61cb072a382a038dc98346d7c33c11b55b11794e
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-07-05 19:27:48 -0400

    D4- *ngFor to display items in a list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- e:any={id:1001, ename:"Kiran", job:"Trainer", salary:5500};    
+ 
+  items: String[] =[];
+  constructor(){
+    this.items = ['pencil', 'pen', 'eraser','staples', 'glue'];
+  }
     
 }
