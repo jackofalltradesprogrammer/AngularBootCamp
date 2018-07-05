07d26f104782b8dc23b1754201d07dddfb38f9a1
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-07-05 19:09:16 -0400

    D4- *ngIf example 2 with use of object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,27 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  solution: number[] = [24,7,365];
-  msg:string;
-  answers: number[] = [0,0,0];
-  score:number = 0;
-  name:string;
- showScore(){
-   if (this.answers[0] == this.solution[0])
-   this.score+=1;
-   if (this.answers[1] == this.solution[1])
-   this.score+=1;
-   if (this.answers[2] == this.solution[2])
-   this.score+=1;
-
-   if (this.score == 3)
-    this.msg = "Passed! " + this.name + "'syour score is " + this.score;
-   else
-   this.msg = "Failed! " + this.name + "'s your score is " + this.score;
-
-
+ e:any={id:1001, ename:"Kiran", job:"Trainer", salary:5500};    
     
-
-  }
-  
 }
