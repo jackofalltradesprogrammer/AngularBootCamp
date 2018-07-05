59047057dbe817b74a740bd2e7a88363954c04b8
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-07-05 19:44:47 -0400

    D4 - Example to display lists of items using *ngFor with buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- 
+  title:string;
   items: String[] =[];
-  constructor(){
-    this.items = ['pencil', 'pen', 'eraser','staples', 'glue'];
+  msg: string;
+  display(p){
+    if(p=="fruits"){
+      this.items = ['grapes', 'mango', 'apple','strawberry', 'blueberry'];
+      this.title = ' List of Vegetables'
+      this.msg = 'Count of Vegetables ' + this.items.length;
+    }
+    else if (p=="vegetables"){
+      this.items = ['potato', 'turnip', 'carrot', 'radish', 'brocolli'];
+      this.title = ' List of Fruits'
+      this.msg = 'Count of Fruits: ' + this.items.length;
+    }
+    
   }
     
 }
