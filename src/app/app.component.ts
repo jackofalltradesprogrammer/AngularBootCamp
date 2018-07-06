50f2cebcbfff3f95d431231cdae6fb2885d99351
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-07-06 00:16:59 -0400

    Day 5 - Prepare the app to retrieve the employees using index property with *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  x:any[]=null;
   employees: any[]=null;
-  getEmployees(){
+  constructor(){
     this.employees = [
       {id:101, ename:'kiran', job:'Trainer', salary:5000},
-      {id:101, ename:'Suhal', job:'Programmer', salary:5000},
-      {id:101, ename:'Pavan', job:'Developer', salary:5000},
-      {id:101, ename:'Jassy', job:'Trainer', salary:5000}
+      {id:102, ename:'Suhal', job:'Programmer', salary:5000},
+      {id:103, ename:'Pavan', job:'Developer', salary:5000},
+      {id:104, ename:'Jassy', job:'Trainer', salary:5000}
     ];
   }
-  clearEmployees(){
-    this.employees =null;
+  selectRow(i){
+    this.x = this.employees[i];
   }
+  
 }
