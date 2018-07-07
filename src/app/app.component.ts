52ed5d2aac47371c29addd82b6ade62b6752af3a
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-07-07 16:10:31 -0400

    D5 - Adding interface (Object oriented features in TypeScript to the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  x:any = {};
-  employees: any[]=[];
+  x = new Employee();
+  employees: IEmployee[]=null;
   msg:string='';
   constructor(){
-    this.employees = [
+    this.employees = new Array<IEmployee>(
       {id:101, ename:'kiran', job:'Trainer', salary:5000},
       {id:102, ename:'Suhal', job:'Programmer', salary:5000},
       {id:103, ename:'Pavan', job:'Developer', salary:5000},
       {id:104, ename:'Jassy', job:'Trainer', salary:5000}
-    ];
+    );
     this.msg = 'Total Employee count is ' + this.employees.length;
   }
   selectRow(i){
@@ -25,25 +25,46 @@ export class AppComponent {
   addRow(){
     this.employees.push(this.x);
     this.msg = 'Total Employee count is ' + this.employees.length;
-    this.x = {};
+    this.x=new Employee();
     alert("Row added succesfully");
   }
   editRow(id){
     for (var index in this.employees) {
       if(id == this.employees[index].id){
         this.employees[index] = this.x;
-        this.x = {};
+        this.x=new Employee();
          alert("Row Edit succesfully");
       }
     }
   }
   deleteRow(i){
     if(confirm("Are you sure?")){
-      this.x={};
-      this.employees.splice(i);
+      this.x=new Employee();
+      this.employees.splice(i,1);
       this.msg = 'Total Employee count is ' + this.employees.length;
       alert("Row Deleted succesfully");
     }
   }
   
+}
+
+interface IEmployee {
+  id:number;
+  ename:string;
+  job:string;
+  salary:number;
+}
+class Employee implements IEmployee {
+  id:number;
+  ename:string;
+  job:string;
+  salary:number;
+  
+  constructor(id?:number, ename?:string, job?:string, salary?:number){
+    this.id=id;
+    this.ename=ename;
+    this.job=job;
+    this.salary=salary;
+  }
+
 }
