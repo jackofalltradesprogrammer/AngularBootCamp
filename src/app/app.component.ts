1a7ef381a78ff0e065bacae0a1dcb08a396dd395
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-07-07 13:59:41 -0400

    D5 - prepare the app for CRUD Operations and use cloning to break the two-way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,37 +8,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   x:any[]=null;
   employees: any[]=null;
-  r:number=0;
-  msg:string=null;
   constructor(){
     this.employees = [
-      {id:101, ename:'Kiran', job:'Trainer', salary:5000},
+      {id:101, ename:'kiran', job:'Trainer', salary:5000},
       {id:102, ename:'Suhal', job:'Programmer', salary:5000},
       {id:103, ename:'Pavan', job:'Developer', salary:5000},
       {id:104, ename:'Jassy', job:'Trainer', salary:5000}
     ];
   }
-  firstRow(){
-    this.r=0;
-    this.x = this.employees[this.r];
+  selectRow(i){
+    // Cloning Json object
+    this.x = JSON.parse(JSON.stringify(this.employees[i]));
   }
-  nextRow(){
-    if(this.r < this.employees.length-1)
-      this.r+=1;
-    else 
-      alert ("No more Next records");
-    this.x = this.employees[this.r];
-  }
-  prevRow(){
-    if(this.r > 0)
-      this.r-=1;
-    else 
-      alert ("No more Previous records");
-    this.x = this.employees[this.r];
-  }
-  lastRow(){
-    this.r=this.employees.length-1;
-    this.x = this.employees[this.r];
-  }
-
+  
 }
