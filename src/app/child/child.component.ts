00f34e8782b2309a6c2d1dfc07fcc5cdea570483
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-07-08 21:05:36 -0400

    D6 - Send data from Child to Parent Component using EventEmitter

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import {Input} from '@angular/core';
+import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css'],
-  inputs:['receivedDataFromParent']
+  inputs:['receivedDataFromParent'],
+  outputs:['childEvent1']
 })
 export class ChildComponent implements OnInit {
 
   // @Input()
   receivedDataFromParent:string;
+  childEvent1 = new EventEmitter<string>();
 
   constructor() { }
+
+  onChange(){
+    this.childEvent1.emit("Hello from Child");
+  }
 
   ngOnInit() {
   }
--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./parent.component.css']
 })
 export class ParentComponent implements OnInit {
-
+  msg:string='';
   getParentData(){
     return "Hello from Parent!";
   }
