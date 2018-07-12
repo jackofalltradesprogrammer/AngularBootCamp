52d19bf7086b140aa21ffbd5115a26388072e570
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-07-12 11:57:15 -0400

    D7 - Style binding by using variable and [style.color] attribute in html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  msg1:string='';
-  msg2:string='';
-  onSubmit=function(obj){
-    this.msg1 = 'Name is ' +obj.name.value;
-    this.msg2= 'City is ' + obj.city.value;
+  titleStyle='BROWN';
   }
   
 }
