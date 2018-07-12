0a9708e8c7bff2a41a164db9c43354901a88bf74
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-07-12 12:32:32 -0400

    D7 - Style binding with functions and using [(ngModel)]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  titleStyle= {'color':'BROWN', 'text-align': 'center' };
+  titleStyle= {
+    "color":"BROWN",
+     "text-align": "center"
+  };
+  termsStyle={};
+  terms:string='';
+  msg:string='';
+  name:string='';
+  check(){
+    if (this.terms){
+      this.msg=this.name +", Thanks for accepting terms";
+      this.termsStyle={"color":"GREEN"};
+    }
+    else{
+      this.msg=this.name +", Please read and accept terms";
+      this.termsStyle={"color":"RED"};
+    }
   }
+}
   
 
 
