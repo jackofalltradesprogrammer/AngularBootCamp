cacd1a915a51d992c38ec56ee620a9ece1064700
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-07-13 19:56:27 -0400

    Day 8 - Pipes demo with numbers and currency symbols

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,27 @@
 import { Component } from '@angular/core';
-import { HelloService } from './hello.service';
+// import { HelloService } from './hello.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers:[HelloService]
+  // providers:[HelloService]
 })
 export class AppComponent {
-  msg:string='';
-  constructor(private _hs:HelloService){
+  p:number;
+  t:number;
+  r:number;
+  si:number;
+  getSimpleInterest(){
+    this.si = ((this.p) * (this.t) * (this.r))/100;
+  }
+  // msg:string='';
+  // constructor(private _hs:HelloService){
     
-  }
-  getHelloMessageFromService(){
-    this.msg = this._hs.getHelloMessage();
-  }
+  // }
+  // getHelloMessageFromService(){
+  //   this.msg = this._hs.getHelloMessage();
+  // }
 }
   
 
