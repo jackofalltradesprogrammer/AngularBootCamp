70138dd3e012286ac2783a3fbcb217c8c1efebb7
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-07-17 14:49:17 -0400

    Day 10 - Using Template driven Forms to pass the form as an object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,14 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
+  employees=[
+    {id:1,ename:"Kiran", job:"Student", salary:"5000"},
+    {id:2,ename:"Happy", job:"Developer", salary:"5000"},
+    {id:3,ename:"Pawan", job:"Trainer", salary:"5000"},
+    {id:4,ename:"Misty", job:"Trainer", salary:"5000"},
+  ];
   onSubmit(obj){
-    console.log(obj);
+    this.employees.push(obj);
   }
 }
   
